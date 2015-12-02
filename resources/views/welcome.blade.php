3e487d56c3fc066f5415c925feacebb263063d32
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-12-02 07:29:30 -0200

    Say the name of project in route /

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Rest Birita</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Rest Birita</div>
+                <p><a href="https://github.com/rogeriopradoj/restbirita">https://github.com/rogeriopradoj/restbirita</a></p>
             </div>
         </div>
     </body>
